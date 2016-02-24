4e0343fdc43f9fec334c198840a8343ab7ab5c76
Vithorio Polten <vithoriop@gmail.com>
2016-02-24 03:52:46 -0300

    o

--- src/sentry_slack/__init__.py
+++ src/sentry_slack/__init__.py
@@ -1,8 +1,3 @@
-try:
-    VERSION = __import__('pkg_resources') \
-        .get_distribution('sentry-slack').version
-except Exception, e:
-    VERSION = 'unknown'
 from __future__ import absolute_import
 
 from sentry.plugins import register
